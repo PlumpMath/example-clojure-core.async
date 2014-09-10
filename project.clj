8493d7181eb2b94aeb39aa48ab38e3e0559148ba
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2014-09-10 22:08:38 +0900

    draft-v0.2

--- project.clj
+++ project.clj
@@ -4,7 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [http-kit "2.1.16"]
+                 [org.clojure/clojurescript "0.0-2322"]]
   :main ^:skip-aot ex-presentation.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]
+                             [lein-cljsbuild "1.0.3"]]
+                   :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                                         :compiler {:output-to "app.js"
+                                                    :optimizations :simple
+                                                    :pretty-print true}}]}}
+             :uberjar {:aot :all}})
--- src/ex_presentation/core.clj
+++ src/ex_presentation/core.clj
@@ -1,8 +1,17 @@
 (ns ex-presentation.core
   (:use [clojure.core.async :exclude [reduce take map into partition merge partition-by]])
   (:require [clojure.core.async :as async]
-            [clojure.pprint :refer [pprint]])
+            [clojure.string     :as str]
+            [org.httpkit.client :as http])
   (:gen-class))
+
+
+(defn line [& worlds]
+  (str (apply str worlds) "\n"))
+
+(defn append-line [to & worlds]
+  (swap! to str (apply line worlds)))
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -30,11 +39,9 @@
 
 (def ch (chan))
 
-(do
-  (thread
-    (Thread/sleep 1000)
-    (>!! ch "something"))
-  (time (println (<!! ch))))
+(do (thread (Thread/sleep 1000)
+            (>!! ch "something"))
+    (time (println (<!! ch))))
 
 (close! ch)
 
@@ -44,10 +51,12 @@
 ;;;
 
 (def ch (chan))
+(def out (atom nil))
 
-(go (println (<! ch)))
+(go (reset! out (<! ch)))
 (go (>! ch "something"))
 
+(println @out)
 (close! ch)
 
 
@@ -59,22 +68,51 @@
 (put! ch nil)
 
 
+;;;
+;;; TITLE:
+;;;
+
+(def ch (chan 1000))
+(def out (atom nil))
+(dotimes [i 1000]
+  (>!! ch i))
+(close! ch)
+
+(dotimes [i 4]
+  (thread (loop []
+            (when-let [v (<!! ch)]
+              (append-line out "thread-" (inc i) ": " v)
+              (recur)))))
+
+(println (count (str/split-lines @out)))
+
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; CHANNEL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;
+;;; TITLE: promise/future
+;;;
+
+(def a-promise (promise))
+(deliver a-promise "something")
+(println @a-promise)
+(println @(future (Thread/sleep 1000)
+                  "something"))
+
 ;;;
 ;;; TITLE: timeout channel
 ;;;
 
-(do
-  (def tch (timeout 1000))
-  (put! tch "something")
-  (println (<!! tch))
-  ;; Channel will be closed after one second...
-  (time (<!! tch)))
+(do (def tch (timeout 1000))
+    (put! tch "something")
+    (println (<!! tch))
+    ;; Channel will be closed after one second...
+    (time (<!! tch)))
 
 
 ;;;
@@ -124,12 +162,18 @@
 ;;;
 
 (def fch (chan 1))
+(def out (atom ""))
+
 (dotimes [i 2]
   (go (>! fch (str "something-" i))
-      (println (str "done " i))))
+      (append-line out "done " i)))
+
+(println @out)
+(println (<!! fch))
+(println @out)
 
 (println (<!! fch))
-(println (<!! fch))
+
 (close! fch)
 
 
@@ -138,11 +182,13 @@
 ;;;
 
 (def dch (chan (dropping-buffer 2)))
+(def out (atom ""))
 
 (dotimes [i 3] (put! dch (str "something-" (inc i))))
 (go (dotimes [i 3]
-      (println (<! dch))))
+      (append-line out (<! dch))))
 
+(println @out)
 (close! dch)
 
 
@@ -151,11 +197,13 @@
 ;;;
 
 (def sch (chan (sliding-buffer 2)))
+(def out (atom ""))
 
 (dotimes [i 3] (put! sch (str "something-" (inc i))))
 (go (dotimes [i 3]
-      (println (<! sch))))
+      (append-line out (<! sch))))
 
+(println @out)
 (close! sch)
 
 
@@ -166,20 +214,22 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;
-;;; TITLE: ...
+;;; TITLE: alts
 ;;;
 
 (def chs (repeatedly 2 chan))
+(def out (atom ""))
 
 (go-loop []
   (let [[v ch] (alts! chs)]
     (when-not (nil? v)
-      (println (str v " from " ch))
+      (append-line out v " from " ch)
       (recur))))
 
 (doseq [[idx ch] (map-indexed vector chs)]
-  (put! ch (str "something-" idx)))
+  (put! ch (str "ch-" idx)))
 
+(println @out)
 (doseq [ch chs] (close! ch))
 
 
@@ -208,16 +258,20 @@
 (def bc-ch (chan))
 (def m (mult bc-ch))
 (def chs (repeatedly 2 chan))
+(def out (atom ""))
 
 (doseq [[idx ch] (map-indexed vector chs)]
   (tap m ch)
   (go-loop []
     (when-let [v (<! ch)]
-      (println (str "ch-" idx ": " v))
+      (append-line out "ch-" idx ": " v)
       (recur))))
 
 (>!! bc-ch "something-1")
+(println @out)
+
 (>!! bc-ch "something-2")
+(println @out)
 
 (doseq [ch chs] (close! ch))
 (close! bc-ch)
@@ -231,21 +285,25 @@
 (def p (pub to-pub :tag))
 (def ch-a (chan))
 (def ch-b (chan))
+(def out (atom ""))
 
 (sub p :a ch-a)
 (go-loop []
   (when-let [v (<! ch-a)]
-    (println "ch-a" v)
+    (append-line out "ch-a: " v)
     (recur)))
 
 (sub p :b ch-b)
 (go-loop []
   (when-let [v (<! ch-b)]
-    (println "ch-b" v)
+    (append-line out "ch-b: " v)
     (recur)))
 
 (>!! to-pub {:tag :a :msg "tag-a"})
+(println @out)
+
 (>!! to-pub {:tag :b :msg "tag-b"})
+(println @out)
 
 (close! ch-a)
 (close! ch-b)
@@ -254,21 +312,47 @@
 
 
 
-(def chs (repeatedly 100 chan))
-(doseq [[idx ch] (map-indexed vector chs)]
-  (go (>! ch (str "something-" idx))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MERGE/TAKE/INTO ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(go-loop []
-  (let [[v ch] (alts! chs)]
-    (when-not (nil? v)
-      (assert (re-matches #"something-[0-9]+" v))
-      (println v)
-      (recur))))
+;;;
+;;; TITLE: merge
+;;;
+
+(def chs (repeatedly 10 chan))
+(doseq [[idx ch] (map-indexed vector chs)]
+  (put! ch idx))
+
+(def ch (async/merge chs))
+(dotimes [i 10]
+  (println (<!! ch)))
 
 (doseq [ch chs] (close! ch))
+(close! ch)
 
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  )
+;;;
+;;; TITLE: take
+;;;
+(def ch (chan 15))
+(dotimes [i 15]
+  (>!! ch i))
+
+(def take-ch (async/take 10 ch))
+(dotimes [i 15]
+  (println (<!! take-ch)))
+
+(close! ch)
+
+
+;;;
+;;; TITLE: into
+;;;
+(def ch (chan 10))
+(dotimes [i 10]
+  (>!! ch i))
+(close! ch)
+
+(def into-ch (async/into [] ch))
+(<!! into-ch)
