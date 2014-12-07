716dd458e7625e50e3723061a5aad951b93adc48
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2014-12-07 23:42:18 +0900

    Use a custom 'println' to redirect a output to the CIDER buffer and Add korean comments(3).

--- src/ex_presentation/core.clj
+++ src/ex_presentation/core.clj
@@ -1,17 +1,34 @@
 (ns ex-presentation.core
-  (:refer-clojure :exclude [println])
+  (:refer-clojure :exclude [println time])
   (:use [clojure.core.async :exclude [reduce take map into partition merge partition-by]])
   (:require [clojure.core.async :as async]
             [clojure.string     :as string]))
 
 ;;; NOTE:
 ;;;  Emacs + CIDER 조합을 사용하면, 출력이 분산되는 문제가 있어서 CIDER 버퍼로 출력을 모으는 꼼수.
-(def println (let [out *out*]
+;;;  그리고, 여러 스레드에서 로그를 출력하다 보니 경쟁 상태가 발생해 "agent"를 사용해 상호배제 시킴.
+(def println (let [ag (agent nil)
+                   out *out*]
                (fn [& more]
                  (binding [*out* out]
-                   (apply clojure.core/println more)))))
+                   (send-off ag #(apply clojure.core/println (rest (flatten %&))) more)
+                   ;; Wait for agent's job finish.
+                   (await ag)
+                   nil))))
+
+;;; NOTE:
+;;;  time 매크로의 출력이 위 println함수와 "*out*"을 두고 경쟁 상태가 발생해서 랩핑 했음.
+(defmacro time [& body]
+  `(let [ret# (atom nil)]
+     (println (with-out-str
+                (reset! ret# (clojure.core/time ~@body))))
+     @ret#))
+
+
+(println "Working thread-safe \"println\" function!")
 
 (throw (Exception. " REPL stop!"))
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -29,6 +46,9 @@
   (close! ch)             ; 채널을 닫는다.
   )
 
+;; => something
+
+
 
 ;;;
 ;;; TITLE: Limitation of callbacks in the channel.
@@ -38,10 +58,12 @@
   ;; 채널에서 기다리는 콜백의 개수가 지정된 숫자를 넘으면 예외가 발생한다.
   (dotimes [i 1025] (take! ch (fn [_]))))
 
+
 (let [ch (chan)]
   ;; 채널에 데이터를 삽입하는 "put!" 함수도 콜백이 있다.
   ;; 디폴트로 아무 일도 안하는 함수가 전달된다.
   (dotimes [i 1025] (put! ch "something")))
+
 
 
 ;;;
@@ -57,7 +79,12 @@
       (take! ch (fn [v]
                   (Thread/sleep 1000)
                   (println v)))
-      (println "waiting until that callback is done!"))))
+      (println "Waiting until that callback is done!"))))
+
+;; => something
+;; => Waiting until that callback is done!
+;; => "Elapsed time: 1006.583748 msecs"
+
 
 (let [ch (chan)]
   (time
@@ -69,47 +96,61 @@
                (Thread/sleep 1000)
                (println v))
              false)
-      (println "waiting until that callback is done!"))))
+      (println "Done!"))))
+
+;; => Done!
+;; => "Elapsed time: 0.835143 msecs"
+;; => something
 
 
 ;;;
 ;;; TITLE: "<!!" and ">!!" function.
 ;;;
 
-;;; TODO: clean up
-
 (let [ch (chan)]
   ;; 끝에 "!!"가 붙은 함수들은 블럭킹이 되는, 즉 동기적으로 실행되는 함수들이다.
+  ;; "<!!"는 "take!" 함수의 콜백이, ">!!"는 "put!" 함수의 콜백이 실행될 때 까지 블럭킹 된다.
   (thread
+    ;; "<!!" 함수를 먼저 실행 시키기 위해서 1초간 대기한다.
     (Thread/sleep 1000)
-    (println "Before to send data to channel.")
-    ;; ">!!" 함수는 "put!" 함수의 블럭킹 버전~
     (>!! ch "something"))
-  (println "Before to receive data from channel.")
-  ;; "<!!" 함수는 "take!" 함수의 블럭킹 버전~
-  (let [output (<!! ch)]
-    (println "Transmitted data from channel:" (str "\"" output "\"")))
-  (println "Done transaction."))
+
+  (time
+    (let [output (<!! ch)]
+      (println "Transmitted data from channel:" (str "\"" output "\"")))))
+
+;; => Transmitted data from channel: "something"
+;; => "Elapsed time: 1004.049221 msecs"
+
 
 
 ;;;
-;;; TITLE: "<!" and ">!" function.
+;;; TITLE: "go" block, "<!" and ">!" function.
 ;;;
 
 (let [ch (chan)]
-  ;; "<!"와 ">!" 함수는 "go" 블럭 안에서만 사용할 수 있다.
-  ;; "go" 블럭 안에서의 "<!"와 ">!" 함수는 마치 "<!!"와 ">!!"와 같이 동작하지만,
-  ;; ...
-  (go
-    (Thread/sleep 1000)
-    (println "1.1 - Before to receive data from channel.")
-    (println "1.2 - Transmitted data from channel:" (str "\"" (<! ch) "\""))
-    (println "1.3 - Done."))
-  (go
-    (println "2.1 - Before to send data to channel.")
-    (>! ch "something")
-    (println "2.2 - Done."))
-  (println "Done transaction."))
+  ;; "go" (매크로)블럭은 비동기적으로 실행된다.
+  ;; "go" 블럭 안에어서는 "<!!"와 ">!!" 함수 대신 블럭킹 되는 것 같이 행동하는 "<!"와 ">!" 함수를 사용해야 한다.
+  ;; 블럭킹 되는 것 같이 행동하는 것을 "파킹"이라고 부른다.
+  (time
+    (go
+      (let [current (. java.lang.System (clojure.core/nanoTime))]
+        (Thread/sleep 1000)
+        (>! ch current))))
+
+  (time
+    (go
+      ;; NOTE:
+      ;;  go 매크로 안에서 커스텀 time 매크로가 사용이 불가해서 전달 시간을 사용함.
+      (let [send-time (<! ch)
+            receive-time (. java.lang.System (clojure.core/nanoTime))
+            delivery-time (/ (double (- receive-time send-time)) 1000000.0)]
+        (println "Delivery time:" delivery-time "msecs")))))
+
+;; => "Elapsed time: 0.126524 msecs"
+;; => "Elapsed time: 0.125212 msecs"
+;; => Delivery time: 1002.052713 msecs
+
 
 
 ;;;
@@ -117,10 +158,14 @@
 ;;;
 
 (let [ch (chan)]
+  ;; "nil"은 특별한 의미를 부여받은 심볼으로 채널에 삽입 할수 없다.
   (try
     (put! ch nil)
     (catch Exception e
       (println "caught exception: " (.getMessage e)))))
+
+;; => caught exception:  Can't put nil on channel
+
 
 
 
@@ -133,7 +178,12 @@
 ;;;
 
 (let [tch (timeout 1000)]
-  (time (<!! tch)))       ; Channel will be closed after one second...
+  ;; 채널으로 데이터 입력이 없어도, 1초 후에 채널이 닫힌다.
+  (time
+    (<!! tch)))
+
+;; => "Elapsed time: 1004.280797 msecs"
+
 
 
 ;;;
@@ -145,6 +195,9 @@
            "result of `thread' block")]
   (println (<!! ch)))
 
+;; => result of `thread' block
+
+
 
 ;;;
 ;;; TITLE: `go' return the channel.
@@ -155,15 +208,21 @@
            "result of `go' block")]
   (println (<!! ch)))
 
+;; => result of `go' block
+
+
 
 ;;;
 ;;; TITLE: closed channel
 ;;;
 
 (let [ch (chan)]
+  ;; 이미 삽입된 데이터는 채널이 닫혔어도 전달 받을 수 있다.
   (put! ch "something from closed channel.")
   (close! ch)
-  (println (<!! ch)))                ; Can read in the closed channel.
+  (println (<!! ch)))
+
+;; => something from closed channel.
 
 
 
@@ -177,24 +236,20 @@
 ;;;
 
 (def fch (chan 1))
-(def out (atom ""))
 
-(go (>! fch (str "something-1"))        ; invoke immediately.
-    (append-line out "done 1"))
+(go
+  (>! fch (str "something-1"))          ; invoke immediately.
+  (println "done 1"))
 
-(println @out)
+(go
+  (>! fch (str "something-2")) ; invoke after taking a data in the channel.
+  (println "done 2"))
 
-(go (>! fch (str "something-2")) ; invoke after taking a data in the channel.
-    (append-line out "done 2"))
-
-(println @out)
-
-(do (append-line out (<!! fch))
-    (println @out))
-
+(println (<!! fch))
 (println (<!! fch))
 
 (close! fch)
+
 
 
 ;;;
@@ -202,16 +257,16 @@
 ;;;
 
 (def dch (chan (dropping-buffer 2)))
-(def out (atom ""))
 
 (dotimes [i 3]
   (>!! dch (str "something-" (inc i)))) ; It was not blocked by dropping the last data.
 
-(go (dotimes [i 3]
-      (append-line out (<! dch))))
+(go
+  (dotimes [i 3]
+    (println (<! dch))))
 
-(println @out)
 (close! dch)
+
 
 
 ;;;
@@ -223,10 +278,10 @@
 
 (dotimes [i 3]
   (>!! sch (str "something-" (inc i)))) ; It was not blocked by dropping the first data.
-(go (dotimes [i 3]
-      (append-line out (<! sch))))
+(go
+  (dotimes [i 3]
+    (println (<! sch))))
 
-(println @out)
 (close! sch)
 
 
@@ -242,22 +297,21 @@
 
 (def ch-1 (chan))
 (def ch-2 (chan))
-(def out (atom ""))
 (def ch-sym->name #({ch-1 "ch-1", ch-2 "ch-2"} %))
 
 (go-loop []
   (let [[v ch] (alts! [ch-1 ch-2])]
     (when-not (nil? v)
-      (append-line out v " from " (ch-sym->name ch))
+      (println v "from" (ch-sym->name ch))
       (recur))))
 
 (>!! ch-1 "something-a")
-(println @out)
 
 (>!! ch-2 "something-b")
-(println @out)
 
-(doseq [ch [ch-1 ch-2]] (close! ch))
+(doseq [ch [ch-1 ch-2]]
+  (close! ch))
+
 
 
 ;;;
@@ -270,6 +324,7 @@
 (println (alts!! [ch] :default :nothing))
 
 (close! ch)
+
 
 
 ;;;
@@ -303,23 +358,22 @@
 (def bc-ch (chan))
 (def m (mult bc-ch))
 (def chs (repeatedly 2 chan))
-(def out (atom ""))
 
 (doseq [[idx ch] (map-indexed vector chs)]
   (tap m ch)
   (go-loop []
     (when-let [v (<! ch)]
-      (append-line out "ch-" idx ": " v)
+      (println "ch-" idx ":" v)
       (recur))))
 
 (>!! bc-ch "something-a")
-(println @out)
 
 (>!! bc-ch "something-b")
-(println @out)
 
-(doseq [ch chs] (close! ch))
+(doseq [ch chs]
+  (close! ch))
 (close! bc-ch)
+
 
 
 ;;;
@@ -330,25 +384,22 @@
 (def p (pub to-pub :tag))
 (def ch-1 (chan))
 (def ch-2 (chan))
-(def out (atom ""))
 
 (sub p :1 ch-1)
 (go-loop []
   (when-let [v (<! ch-1)]
-    (append-line out "ch-1: " v)
+    (println "ch-1: " v)
     (recur)))
 
 (sub p :2 ch-2)
 (go-loop []
   (when-let [v (<! ch-2)]
-    (append-line out "ch-2: " v)
+    (println "ch-2: " v)
     (recur)))
 
 (>!! to-pub {:tag :1 :msg "tag-1"})
-(println @out)
 
 (>!! to-pub {:tag :2 :msg "tag-2"})
-(println @out)
 
 (close! ch-1)
 (close! ch-2)
@@ -377,6 +428,7 @@
 (close! ch)
 
 
+
 ;;;
 ;;; TITLE: take
 ;;;
@@ -390,6 +442,7 @@
   (println (<!! take-ch)))
 
 (close! ch)
+
 
 
 ;;;
